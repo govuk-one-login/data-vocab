ec0e4552e032ef631afc1b9bcefcfe7c408dcaec
Pete Cornish <474202+pcornish@users.noreply.github.com>
2023-09-04 17:35:37 +0100

    ci: publish types to GitHub packages.

--- scripts/generate_types.sh
+++ scripts/generate_types.sh
@@ -6,9 +6,3 @@ ROOT_DIR="$( git rev-parse --show-toplevel )"
 cd "${ROOT_DIR}/type-generator"
 npm ci
 npm run generate
-
-TYPES_ARCHIVE="${ROOT_DIR}/v1/typescript-types.zip"
-cd "${ROOT_DIR}/v1/types"
-zip -r "${TYPES_ARCHIVE}" .
-
-echo "Archived types to ${TYPES_ARCHIVE}"
