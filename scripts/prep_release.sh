4e764d7f34d1855e5abe46532462aaf7e4f378d1
Pete Cornish <474202+pcornish@users.noreply.github.com>
2023-09-14 14:38:25 +0100

    build: adds convenience script for preparing a release.

---
+++ scripts/prep_release.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ $# -lt 1 ]]; then
+  echo "Missing release type. Must be one of major, minor or patch"
+  exit 1
+else
+  RELEASE_TYPE="$1"
+fi
+
+cd "$( git rev-parse --show-toplevel )"
+
+npm version "$RELEASE_TYPE" --git-tag-version=false
+
+# update lockfile etc.
+npm install
+
+NEW_VERSION="$( npm pkg get version | sed 's/"//g' )"
+echo -e "New version: ${NEW_VERSION}\nCommit changes and create tag (y/N)?"
+read -r CONFIRM_COMMIT
+if [[ -z "$CONFIRM_COMMIT" || "y" != "$CONFIRM_COMMIT" ]]; then
+  echo "Aborted"
+  exit 1
+fi
+
+git commit \
+  package.json \
+  package-lock.json \
+  -m"build: release ${NEW_VERSION}."
+
+git tag "v$NEW_VERSION"
