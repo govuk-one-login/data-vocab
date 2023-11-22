8083f9949e4b724383d43ecbf30a699f7390c587
Dominic Stevenson <dominic.stevenson@digital.cabinet-office.gov.uk>
2023-11-22 11:39:51 +0000

    Remove repurposed slots from audit events nav bar

--- scripts/build_nav.py
+++ scripts/build_nav.py
@@ -15,7 +15,7 @@ CUSTOM_NAMES = {
     "audit-events/json-schemas/index.md": "JSON Schema files",
     "index.md": "1. GOV.UK One Login Vocabulary",
     "v1/_Home/index.md": "credentials",
-    "v1/json-schemas/index.md": "JSON Schema files"
+    "v1/json-schemas/index.md": "JSON Schema files",
 }
 
 CUSTOM_CATEGORY_NAMES = {
@@ -24,20 +24,26 @@ CUSTOM_CATEGORY_NAMES = {
     "types": "Types",
     "classes": "Classes",
     "enums": "Enums",
-    "json-schemas": "JSON Schemas", 
+    "json-schemas": "JSON Schemas",
     "audit-events": "3. Audit Events",
     "v1": "2. Physical Data Model",
 }
 
+
 def pascal_to_upper_snake(pascal_str):
     """Convert a PascalCase string to UPPER_SNAKE_CASE."""
-    return ''.join(['_' + i.lower() if i.isupper() else i for i in pascal_str]).lstrip('_').upper()
+    return (
+        "".join(["_" + i.lower() if i.isupper() else i for i in pascal_str])
+        .lstrip("_")
+        .upper()
+    )
 
 
 def is_redirect(file_path):
     """Check if the given file path is a redirect."""
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         return file.readline().strip() == "# Redirect"
+
 
 def add_to_structure(path_parts, nav_list, file_path):
     """Recursively process and add the given path to the navigation structure."""
@@ -60,24 +66,26 @@ def add_to_structure(path_parts, nav_list, file_path):
 
     add_to_structure(path_parts, item[part], file_path)
 
+
 def sort_nav_structure(nav_list):
     """
     Sort the given navigation structure.
-    
+
     - Home appears on top
     - Everything else appears alphabetically
     """
 
-    home_item = [item for item in nav_list if 'Home' in item]
-    other_items = [item for item in nav_list if 'Home' not in item]
+    home_item = [item for item in nav_list if "Home" in item]
+    other_items = [item for item in nav_list if "Home" not in item]
 
-    sorted_other_items = sorted(other_items, key=lambda item: list(item.keys())[0].lower())
+    sorted_other_items = sorted(
+        other_items, key=lambda item: list(item.keys())[0].lower()
+    )
 
     for item in sorted_other_items:
         for key, value in item.items():
             if isinstance(value, list):
                 item[key] = sort_nav_structure(value)
-
 
     sorted_nav_list = home_item + sorted_other_items
 
@@ -88,10 +96,9 @@ def generate_navigation_structure():
     """Generate and return the navigation structure based on the DOCS_DIR."""
     nav_structure = []
 
-
     for root, dirs, files in os.walk(DOCS_DIR, followlinks=True):
         for file in files:
-            if file.endswith('.md'):
+            if file.endswith(".md"):
                 full_path = os.path.join(root, file)
                 print(full_path)
                 if is_redirect(full_path):
@@ -99,23 +106,35 @@ def generate_navigation_structure():
                 rel_path = os.path.relpath(full_path, DOCS_DIR)
                 path_parts = rel_path.split(os.sep)
 
-          
-                if 'audit-events' in path_parts and 'classes' in path_parts and 'AuditEventClass.md' not in file:
-
+                # Custom logic for audit events
+                # Only show the root audit event class for each event in the nav
+                if (
+                    "audit-events" in path_parts
+                    and "classes" in path_parts
+                    and "AuditEventClass.md" not in file
+                ):
                     continue
-                if 'audit-events' in path_parts and 'classes' in path_parts:
-                    path_parts[1] = 'Events'
-                    path_parts[-1] = pascal_to_upper_snake(path_parts[-1].replace('AuditEventClass.md', ''))
 
-        
+                # Replace class names with event names in the nav bar
+                if "audit-events" in path_parts and "classes" in path_parts:
+                    path_parts[1] = "Events"
+                    path_parts[-1] = pascal_to_upper_snake(
+                        path_parts[-1].replace("AuditEventClass.md", "")
+                    )
 
+                # Don't add slots which have been updated in slot_usage within class files to the Nav
+                if (
+                    "audit-events" in path_parts
+                    and "slots" in path_parts
+                    and "AuditEventClass" in file
+                ):
+                    continue
 
                 if rel_path in CUSTOM_NAMES:
                     path_parts[-1] = CUSTOM_NAMES[rel_path]
                 else:
-                    path_parts[-1] = path_parts[-1].replace('.md', '')
+                    path_parts[-1] = path_parts[-1].replace(".md", "")
                 add_to_structure(path_parts, nav_structure, rel_path)
-
 
     return sort_nav_structure(nav_structure)
 
@@ -124,17 +143,17 @@ def update_mkdocs_config(nav_structure):
     """Update the mkdocs.yml file with the given navigation structure."""
     yaml = ruamel.yaml.YAML()
     yaml.indent(mapping=2, sequence=4, offset=2)
-    with open('mkdocs.yml', 'r') as file:
+    with open("mkdocs.yml", "r") as file:
         mkdocs_data = yaml.load(file)
-    mkdocs_data['nav'] = nav_structure
-    with open('mkdocs.yml', 'w') as file:
+    mkdocs_data["nav"] = nav_structure
+    with open("mkdocs.yml", "w") as file:
         yaml.dump(mkdocs_data, file)
+
 
 def main():
     nav_structure = generate_navigation_structure()
     update_mkdocs_config(nav_structure)
 
+
 if __name__ == "__main__":
     main()
-
-
