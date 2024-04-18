c295c88890bc61c88535dac99c5504fc0fb6b0f5
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-04-18 16:03:29 +0100

    feat: Add interfaces for credential JWTs and classes

--- code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomRuleFactory.java
+++ code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomRuleFactory.java
@@ -1,13 +1,28 @@
 package uk.gov.di.codegen;
 
 import com.sun.codemodel.JClassContainer;
+import com.sun.codemodel.JPackage;
 import com.sun.codemodel.JType;
 import org.jsonschema2pojo.rules.Rule;
 import org.jsonschema2pojo.rules.RuleFactory;
+import org.jsonschema2pojo.util.ParcelableHelper;
+import org.jsonschema2pojo.util.ReflectionHelper;
 
 public class CustomRuleFactory extends RuleFactory {
+  private final ReflectionHelper reflectionHelper;
+
+  public CustomRuleFactory() {
+    super();
+    this.reflectionHelper = new ReflectionHelper(this);
+  }
+
   @Override
   public Rule<JClassContainer, JType> getSchemaRule() {
     return new CustomSchemaRule(this);
+  }
+
+  @Override
+  public Rule<JPackage, JType> getObjectRule() {
+    return new CustomObjectRule(this, new ParcelableHelper(), reflectionHelper);
   }
 }
