a3d9ecd572d1b37ead5d6dde906e9acd698474fb
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-04-18 18:51:33 +0100

    feat: Strip Class suffix from generated types

--- code-generators/java-types/jackson-annotated/src/test/java/uk/gov/di/model/ModelTest.java
+++ code-generators/java-types/jackson-annotated/src/test/java/uk/gov/di/model/ModelTest.java
@@ -28,20 +28,20 @@ class ModelTest {
 
     @Test
     public void useModel() throws Exception {
-        var credentials = new IdentityCheckCredentialJWTClass();
+        var credentials = new IdentityCheckCredentialJWT();
         credentials.setSub("urn:fdc:gov.uk:2022:954bc117-731b-41cd-86cf-dfb4e7940fce");
         credentials.setAud("https://passport.core.stubs.account.gov.uk");
         credentials.setNbf(1690816091);
         credentials.setIss("https://review-p.build.account.gov.uk");
 
-        var vc = new IdentityCheckCredentialClass();
+        var vc = new IdentityCheckCredential();
         vc.setType(List.of(
                 VerifiableCredentialType.VERIFIABLE_CREDENTIAL,
                 VerifiableCredentialType.IDENTITY_CHECK_CREDENTIAL
         ));
         credentials.setVc(vc);
 
-        var evidence = new IdentityCheckClass();
+        var evidence = new IdentityCheck();
         evidence.setValidityScore(0);
         evidence.setStrengthScore(4);
         evidence.setCi(List.of("D02"));
@@ -49,28 +49,28 @@ class ModelTest {
         evidence.setType("IdentityCheck");
         vc.setEvidence(List.of(evidence));
 
-        var credentialSubject = new IdentityCheckSubjectClass();
+        var credentialSubject = new IdentityCheckSubject();
         vc.setCredentialSubject(credentialSubject);
 
-        var passport = new PassportDetailsClass();
+        var passport = new PassportDetails();
         passport.setExpiryDate("2030-12-12");
         passport.setIcaoIssuerCode("GBR");
         passport.setDocumentNumber("123456789");
         credentialSubject.setPassport(List.of(passport));
 
-        var givenName = new NamePartClass();
-        givenName.setType(NamePartClass.NamePartType.GIVEN_NAME);
+        var givenName = new NamePart();
+        givenName.setType(NamePart.NamePartType.GIVEN_NAME);
         givenName.setValue("Kenneth");
 
-        var familyName = new NamePartClass();
-        familyName.setType(NamePartClass.NamePartType.FAMILY_NAME);
+        var familyName = new NamePart();
+        familyName.setType(NamePart.NamePartType.FAMILY_NAME);
         familyName.setValue("Decerqueira");
 
-        var name = new NameClass();
+        var name = new Name();
         name.setNameParts(List.of(givenName, familyName));
         credentialSubject.setName(List.of(name));
 
-        var birthDate = new BirthDateClass();
+        var birthDate = new BirthDate();
         birthDate.setValue("1990-01-23");
         credentialSubject.setBirthDate(List.of(birthDate));
 
