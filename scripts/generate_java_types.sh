2798fee11b4df54b8b255bee136eea04f7ab4e30
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-04-18 14:21:32 +0100

    feat: generate Java types using combined credentials schema

--- scripts/generate_java_types.sh
+++ scripts/generate_java_types.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$ROOT_DIR" ]]; then
-  ROOT_DIR="$( git rev-parse --show-toplevel )"
-fi
+ROOT_DIR="$( git rev-parse --show-toplevel )"
+LINKML_SCHEMA_DIR="${ROOT_DIR}/v1/linkml-schemas"
+JSON_SCHEMA_DIR="${ROOT_DIR}/code-generators/java-types/schemas"
+
+# Generate intermediate json-schema used for the Java type generation
+mkdir -p $JSON_SCHEMA_DIR
+poetry run gen-json-schema --closed --no-metadata "${LINKML_SCHEMA_DIR}/credentials.yaml" > "${JSON_SCHEMA_DIR}/credentials.json"
 
 cd "${ROOT_DIR}/code-generators/java-types"
 ./gradlew clean build
