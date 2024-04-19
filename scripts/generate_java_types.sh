3689ba936f4e1a3cd90be13ccea3ccff2383ea01
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-04-19 13:27:09 +0100

    feat: generate java types with inheritance

--- scripts/generate_java_types.sh
+++ scripts/generate_java_types.sh
@@ -7,7 +7,7 @@ JSON_SCHEMA_DIR="${ROOT_DIR}/code-generators/java-types/schemas"
 
 # Generate intermediate json-schema used for the Java type generation
 mkdir -p $JSON_SCHEMA_DIR
-poetry run gen-json-schema --closed --no-metadata "${LINKML_SCHEMA_DIR}/credentials.yaml" > "${JSON_SCHEMA_DIR}/credentials.json"
+poetry run gen-json-schema --closed --no-metadata --nonstandard-extends "${LINKML_SCHEMA_DIR}/credentials.yaml" > "${JSON_SCHEMA_DIR}/credentials.json"
 
 cd "${ROOT_DIR}/code-generators/java-types"
 ./gradlew clean build
