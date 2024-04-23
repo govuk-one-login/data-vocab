a731ef1632abeeba78fea53764e2177432d02a5f
Pete Cornish <474202+pcornish@users.noreply.github.com>
2024-04-23 18:29:45 +0100

    build: allows script root dir to be set by parent environment.

--- scripts/generate_java_types.sh
+++ scripts/generate_java_types.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT_DIR="$( git rev-parse --show-toplevel )"
+if [[ -z "$ROOT_DIR" ]]; then
+  ROOT_DIR="$( git rev-parse --show-toplevel )"
+fi
 
 cd "${ROOT_DIR}/code-generators/java-types"
 ./gradlew clean build
