b36002c75ed9ff59ce260f8787aa915dfa16d82c
Pete Cornish <474202+pcornish@users.noreply.github.com>
2024-04-25 11:54:45 +0100

    build: moves JSON schema generation to single script, with options.

--- scripts/generate_java_types.sh
+++ scripts/generate_java_types.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT_DIR="$( git rev-parse --show-toplevel )"
-LINKML_SCHEMA_DIR="${ROOT_DIR}/v1/linkml-schemas"
+if [[ -z "$ROOT_DIR" ]]; then
+  ROOT_DIR="$( git rev-parse --show-toplevel )"
+fi
 JSON_SCHEMA_DIR="${ROOT_DIR}/code-generators/java-types/schemas"
 
 # Generate intermediate json-schema used for the Java type generation
-mkdir -p $JSON_SCHEMA_DIR
-poetry run gen-json-schema --closed --no-metadata --nonstandard-extends "${LINKML_SCHEMA_DIR}/credentials.yaml" > "${JSON_SCHEMA_DIR}/credentials.json"
+cd "${ROOT_DIR}/scripts"
+./generate_json_schemas.sh -j "$JSON_SCHEMA_DIR" -o combined
 
 cd "${ROOT_DIR}/code-generators/java-types"
 ./gradlew clean build
