e375a2878b45e349b6dcc3fef8b144a0e41e7ceb
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2024-05-20 13:55:43 +0100

    SPT-808: Add getSchemaAsString and other review comments
    - Add a `getSchemaAsString()` method
    - Add schema name constants
    - Re-order `settings.gradle`
    - Use artefactory repository

--- code-generators/java-types/schema-artefacts/src/test/java/uk/gov/di/model/schema/VocabSchemaLoaderTest.java
+++ code-generators/java-types/schema-artefacts/src/test/java/uk/gov/di/model/schema/VocabSchemaLoaderTest.java
@@ -1,24 +1,59 @@
 package uk.gov.di.model.schema;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.di.model.schema.VocabSchemaLoader.IDENTITY_CHECK_CREDENTIAL_JWT_NAME;
+import static uk.gov.di.model.schema.VocabSchemaLoader.INHERITED_IDENTITY_JWT_NAME;
+import static uk.gov.di.model.schema.VocabSchemaLoader.RISK_ASSESSMENT_CREDENTIAL_JWT_NAME;
+import static uk.gov.di.model.schema.VocabSchemaLoader.SECURITY_CHECK_CREDENTIAL_JWT_NAME;
 
 class VocabSchemaLoaderTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "IdentityCheckCredentialJWT.json",
-            "InheritedIdentityJWT.json",
-            "RiskAssessmentCredentialJWT.json",
-            "SecurityCheckCredentialJWT.json",
+            IDENTITY_CHECK_CREDENTIAL_JWT_NAME,
+            INHERITED_IDENTITY_JWT_NAME,
+            RISK_ASSESSMENT_CREDENTIAL_JWT_NAME,
+            SECURITY_CHECK_CREDENTIAL_JWT_NAME,
     })
-    void getSchema(String schemaName) throws IOException {
+    void shouldReturnInputStreamWhenPassedValidSchemaNames(String schemaName) throws IOException {
         try (var schema = VocabSchemaLoader.getSchema(schemaName)) {
             assertNotNull(schema);
         }
+    }
+
+    @Test
+    void shouldReturnNullStreamWhenPassedInvalidSchemaName() {
+        assertNull(VocabSchemaLoader.getSchema("does-not-exist.json"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            IDENTITY_CHECK_CREDENTIAL_JWT_NAME,
+            INHERITED_IDENTITY_JWT_NAME,
+            RISK_ASSESSMENT_CREDENTIAL_JWT_NAME,
+            SECURITY_CHECK_CREDENTIAL_JWT_NAME,
+    })
+    void shouldReturnSchemaAsStringWhenPassedValidSchemaNames(String schemaName) {
+        var schema = VocabSchemaLoader.getSchemaAsString(schemaName);
+
+        assertNotNull(schema);
+        assertTrue(schema.trim().startsWith("{"));
+        assertTrue(schema.trim().endsWith("}"));
+    }
+
+    @Test
+    void shouldReturnNullStringWhenPassedInvalidSchemaName() {
+        assertNull(VocabSchemaLoader.getSchemaAsString("does-not-exist.json"));
     }
 }
