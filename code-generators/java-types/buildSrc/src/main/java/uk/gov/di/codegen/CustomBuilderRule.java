87a1fee622cbf1c5ed879716c192a99ffa9c2e3c
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-03-12 14:46:54 +0000

    SPT-1296: Added documentation to CustomBuilderRule
    Added documentation to CustomBuilderRule which explains what it's for
    and what the code is base from.

--- code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomBuilderRule.java
+++ code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomBuilderRule.java
@@ -11,6 +11,7 @@ import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JInvocation;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
 import com.sun.codemodel.JVar;
 import org.jsonschema2pojo.Schema;
 import org.jsonschema2pojo.rules.Rule;
@@ -20,7 +21,18 @@ import org.jsonschema2pojo.util.ReflectionHelper;
 import java.util.Objects;
 
 /**
- * Custom implementation of the BuilderRule which generates a Builder without needing generics.
+ * Custom implementation of the {@link org.jsonschema2pojo.rules.BuilderRule}
+ * which generates a more simplified Builder without using generics.
+ * <p />
+ * Ideally this rule would have extended from the {@link org.jsonschema2pojo.rules.BuilderRule}
+ * however, unlike most of the other rules implemented within the jsonschema2pojo
+ * library the <code>BuilderRule</code> constructor is package protected. The
+ * code except the {@link uk.gov.di.codegen.CustomBuilderRule#apply(String, JsonNode, JsonNode, JDefinedClass, Schema)}
+ * method is identical to the original {@link org.jsonschema2pojo.rules.BuilderRule}
+ * implementation.
+ * <p />
+ *
+ * @see <a href="https://github.com/joelittlejohn/jsonschema2pojo/blob/master/jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/rules/BuilderRule.java">org.jsonschema2pojo.rules.BuilderRule</a>
  */
 public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
     private final RuleFactory ruleFactory;
@@ -38,8 +50,8 @@ public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
         try {
             String concreteBuilderClassName = this.ruleFactory.getNameHelper().getBuilderClassName(instanceClass);
             String builderClassName = this.ruleFactory.getNameHelper().getBaseBuilderClassName(instanceClass);
-            builderClass = instanceClass._class(49, builderClassName);
-            concreteBuilderClass = instanceClass._class(17, concreteBuilderClassName);
+            builderClass = instanceClass._class(JMod.PUBLIC | JMod.ABSTRACT | JMod.STATIC, builderClassName);
+            concreteBuilderClass = instanceClass._class(JMod.PUBLIC | JMod.STATIC, concreteBuilderClassName);
             concreteBuilderClass._extends(builderClass);
         } catch (JClassAlreadyExistsException e) {
             return e.getExistingClass();
@@ -51,11 +63,11 @@ public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
             parentBuilderClass = this.reflectionHelper.getBaseBuilderClass(parentClass);
         }
 
-        JMethod buildMethod = builderClass.method(1, instanceClass, "build");
+        JMethod buildMethod = builderClass.method(JMod.PUBLIC, instanceClass, "build");
         JBlock body = buildMethod.body();
 
         if (parentBuilderClass == null) {
-            JFieldVar instanceField = builderClass.field(2, instanceClass, "instance");
+            JFieldVar instanceField = builderClass.field(JMod.PROTECTED, instanceClass, "instance");
             JVar result = body.decl(instanceClass, "result");
             body.assign(result, JExpr._this().ref(instanceField));
             body.assign(JExpr._this().ref(instanceField), JExpr._null());
@@ -68,7 +80,7 @@ public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
             this.generateNoArgsBuilderConstructors(instanceClass, builderClass, concreteBuilderClass);
         }
 
-        JMethod builderMethod = instanceClass.method(17, builderClass, "builder");
+        JMethod builderMethod = instanceClass.method(JMod.PUBLIC | JMod.STATIC, builderClass, "builder");
         JBlock builderBody = builderMethod.body();
         builderBody._return(JExpr._new(concreteBuilderClass));
         return builderClass;
@@ -80,7 +92,7 @@ public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
     }
 
     private void generateNoArgsBuilderConstructor(JDefinedClass baseBuilderClass, JDefinedClass builderClass) {
-        JMethod noArgsConstructor = builderClass.constructor(1);
+        JMethod noArgsConstructor = builderClass.constructor(JMod.PUBLIC);
         JBlock constructorBlock = noArgsConstructor.body();
         if (!baseBuilderClass.isPrimitive() && !this.reflectionHelper.isFinal(baseBuilderClass) && !Objects.equals(baseBuilderClass.fullName(), OBJECT_CLASS_NAME)) {
             constructorBlock.invoke("super");
@@ -88,7 +100,7 @@ public class CustomBuilderRule implements Rule<JDefinedClass, JDefinedClass> {
     }
 
     private void generateNoArgsBaseBuilderConstructor(JDefinedClass instanceClass, JDefinedClass builderClass, JDefinedClass concreteBuilderClass) {
-        JMethod noArgsConstructor = builderClass.constructor(1);
+        JMethod noArgsConstructor = builderClass.constructor(JMod.PUBLIC);
         JAnnotationUse warningSuppression = noArgsConstructor.annotate(SuppressWarnings.class);
         warningSuppression.param("value", "unchecked");
         JBlock constructorBlock = noArgsConstructor.body();
