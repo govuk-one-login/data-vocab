ab111a1077e472a79921493af743579fa40d3dfe
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2025-03-12 14:04:23 +0000

    SPT-1296: Added doc to CustomPropertyRule
    * Added documentation to CustomPropertyRule which explains what the
      class is doing and why.
    * Small refactor to CustomPropertyRule to simplify how we check if the
      field was in the parent. Previously all the information about the
      field was provided, however the test only needed to know if the field
      was present or not.

--- code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomPropertyRule.java
+++ code-generators/java-types/buildSrc/src/main/java/uk/gov/di/codegen/CustomPropertyRule.java
@@ -8,6 +8,21 @@ import org.jsonschema2pojo.Schema;
 import org.jsonschema2pojo.rules.PropertyRule;
 import org.jsonschema2pojo.rules.RuleFactory;
 
+/**
+ * The JSON Schema contains definitions for all classes and there definitions.
+ * However, many of the class definitions contain fields which are also defined
+ * in their super classes. This results in POJO being defined in both
+ * superclasses and subclasses, if these are defined as being identical or
+ * where the type of the property extends from the type of the superclass
+ * property.
+ * <p />
+ * This {@link org.jsonschema2pojo.rules.PropertyRule} looks at each classes
+ * super class to see if the parent contains a field of the same name. If the
+ * property is found it will just return the defined class as is, otherwise it
+ * uses the default behaviour of
+ * {@link org.jsonschema2pojo.rules.PropertyRule#apply(String, JsonNode, JsonNode, JDefinedClass, Schema)}
+ * to create the field, setters, getters and builder methods.
+ */
 public class CustomPropertyRule extends PropertyRule {
     private final RuleFactory ruleFactory;
 
@@ -20,25 +35,30 @@ public class CustomPropertyRule extends PropertyRule {
     public JDefinedClass apply(String nodeName, JsonNode node, JsonNode parent, JDefinedClass jclass, Schema schema) {
         String propertyName = this.ruleFactory.getNameHelper().getPropertyName(nodeName, node);
 
-        FieldTuple jFieldVar = getParentField(propertyName, jclass);
-        if (jFieldVar != null) {
+        if (isDefinedInParent(propertyName, jclass)) {
             return jclass;
         }
 
         return super.apply(nodeName, node, parent, jclass, schema);
     }
 
-    private FieldTuple getParentField(String fieldName, JDefinedClass jclass) {
+    /**
+     * Iterates through the class hierarchy to determine whether the field is
+     * defined in a parent class.
+     *
+     * @param fieldName The name of the field to look for
+     * @param jclass The class to look at the parent field
+     * @return true if the field is found
+     */
+    private boolean isDefinedInParent(String fieldName, JDefinedClass jclass) {
         JClass jParentClass = jclass._extends();
         if (jParentClass instanceof JDefinedClass jDefinedParentClass) {
             JFieldVar jFieldVar = jDefinedParentClass.fields().get(fieldName);
             if (jFieldVar != null) {
-                return new FieldTuple(jDefinedParentClass, jFieldVar);
+                return true;
             }
-            return getParentField(fieldName, jDefinedParentClass);
+            return isDefinedInParent(fieldName, jDefinedParentClass);
         }
-        return null;
+        return false;
     }
-
-    private record FieldTuple(JDefinedClass owner, JFieldVar fieldVar) {}
 }
